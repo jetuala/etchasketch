ea59f268747306c5d34e8e3462d2ce2a617df1c1
Jonathan Tuala <jetuala@gmail.com>
2021-06-09 13:09:51 -0700

    Basic functionality

---
+++ script.js
@@ -0,0 +1,16 @@
+// make row of 16 divs
+
+let mainSketchContainer = document.getElementById("mainSketchContainer");
+
+for (let j = 0; j < 16; j++) {
+    for (let i = 0; i < 16; i++) {
+        let div = document.createElement('div');
+        div.id = "row" + j + "col" + i;
+        div.className = "block";
+        div.addEventListener("mouseover", function() {
+            div.classList.add("drawn");
+        })
+
+        mainSketchContainer.append(div);
+    }
+}
